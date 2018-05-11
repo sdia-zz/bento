cd0c8e9c0038eed0f8780d691567db5dd98a3d2c
Seydou Dia <seydou.dia@olx.com>
2018-05-11 19:43:20 +0200

    initial commit.

---
+++ api/config/config.exs
@@ -0,0 +1,23 @@
+# This file is responsible for configuring your application
+# and its dependencies with the aid of the Mix.Config module.
+#
+# This configuration file is loaded before any dependency and
+# is restricted to this project.
+use Mix.Config
+
+# Configures the endpoint
+config :api, ApiWeb.Endpoint,
+  url: [host: "localhost"],
+  secret_key_base: "nKArgQypTK2KLEnA6P3pDBalL589l/2Hff5O4XF/AX4MAVPg+ByZFs+8fhhLLFfP",
+  render_errors: [view: ApiWeb.ErrorView, accepts: ~w(html json)],
+  pubsub: [name: Api.PubSub,
+           adapter: Phoenix.PubSub.PG2]
+
+# Configures Elixir's Logger
+config :logger, :console,
+  format: "$time $metadata[$level] $message\n",
+  metadata: [:request_id]
+
+# Import environment specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+import_config "#{Mix.env}.exs"
